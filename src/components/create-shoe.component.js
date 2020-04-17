81c3a31f278b69510ad7c20bf40e7226cbc0fa1e
Kevin Matthews <kevmatthews3@gmail.com>
2020-04-17 02:30:51 -0400

    create-form

--- src/components/create-shoe.component.js
+++ src/components/create-shoe.component.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
 
 export default class CreateShoe extends Component {
     render() {
-        return (
-            <div>
-            <p>React Create Shoe Component!</p>
-            </div>
-        );
+        return (<div class="form-wrapper">
+        <Form>
+        <Form.Group controlId="Name">
+            <Form.Label>Shoe-Name</Form.Label>
+            <Form.Control type="text"/>
+        </Form.Group>
+
+        <Form.Group controlId="Email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email"/>
+        </Form.Group>
+
+        <Form.Group controlId="Name">
+            <Form.Label>Id No</Form.Label>
+            <Form.Control type="text"/>
+        </Form.Group>
+
+<Button variant="danger" size="lg" block="block" type="submit">
+Create Shoe Style
+</Button>
+
+        </Form>
+        </div>);
     }
 }
+    
